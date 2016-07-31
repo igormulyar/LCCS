3792767744990bd68caa40c5d7f6541b028c63e7
igormulyar <frutyloops1004>
2016-07-31 13:18:26 +0300

    some shit

--- src/main/java/ExcelHandler.java
+++ src/main/java/ExcelHandler.java
@@ -1,3 +1,4 @@
+import org.apache.poi.hssf.record.pivottable.StreamIDRecord;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -7,6 +8,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by TRUST on 29.07.2016.
@@ -14,20 +17,27 @@ import java.io.IOException;
 public class ExcelHandler {
 
     private String filePath = "C:\\MyTable.xls";
+    private String sheetName = "Лист3";
 
 
     public ExcelHandler() throws IOException {
-        File file = new File(filePath);
-        HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream(file));
-        HSSFSheet myExcelSheet = myExcelBook.getSheet("Лист3");
-        HSSFRow row = myExcelSheet.getRow(1);
 
-        if(row.getCell(0).getCellType() == HSSFCell.CELL_TYPE_STRING){
-            String name = row.getCell(0).getStringCellValue();
-            System.out.println("name : " + name);
-        } else {
-            System.out.println("Something wrong with cell type.");
+        HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream(new File(filePath)));
+        HSSFSheet myExcelSheet = myExcelBook.getSheet(sheetName);
+        HSSFRow row = myExcelSheet.getRow(1);
+         System.out.println("Last column number:"+myExcelSheet.getLastRowNum());
+        List<String> IDList = new ArrayList<String>();
+
+        for (int i=1; i<=myExcelSheet.getLastRowNum(); i++){
+            if(row.getCell(0).getCellType() == HSSFCell.CELL_TYPE_STRING){
+                String ID = row.getCell(0).getStringCellValue();
+                System.out.println("ID #\""+i+"\" : " + ID);
+                IDList.add(ID);
+            } else {
+                System.out.println("Something wrong with cell type. Uncorrect cell type.");
+            }
         }
+
         myExcelBook.close();
     }
 
