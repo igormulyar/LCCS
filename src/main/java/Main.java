7ee07904c59439489ba0bb4b27c3dba14c1ed721
igormulyar <frutyloops1004>
2016-08-01 21:34:36 +0300

    some newer shit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,11 @@ public class Main {
             System.out.println(s);
         }
 
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://github.com/Vedenin/useful-java-links/tree/master/link-rus#%D0%91%D0%B5%D0%B7%D0%BE%D0%BF%D0%B0%D1%81%D0%BD%D0%BE%D1%81%D1%82%D1%8C-%D0%B8-%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F");
+        System.out.println(driver.getTitle());
+        driver.close();
+        driver.quit();
 
     }
 }
