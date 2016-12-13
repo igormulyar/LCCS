b2fa7b519c0c1e6aa4ade09db0f3798e9b20c17c
Vladislav Yevsikov <v.yevsikov@gmail.com>
2016-12-13 09:50:10 +0200

    first frontend version

---
+++ src/main/java/view/html/table.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  ready();
+});
+
+function ready() {
+  var headertext = [],
+  headers = document.querySelectorAll("#law th"),
+  tablerows = document.querySelectorAll("#law th"),
+  tablebody = document.querySelector("#law tbody");
+
+  for(var i = 0; i < headers.length; i++) {
+    var current = headers[i];
+    headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+  }
+  for (var i = 0, row; row = tablebody.rows[i]; i++) {
+    for (var j = 0, col; col = row.cells[j]; j++) {
+      col.setAttribute("data-th", headertext[j]);
+    }
+  }
+}
