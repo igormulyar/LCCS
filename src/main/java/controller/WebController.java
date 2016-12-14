e7c221dce408fe56a9deadde570013b47f6d7598
igormulyar <igormulyar@ukr.net>
2016-12-14 09:00:38 +0200

    web - it works!

--- src/main/java/controller/WebController.java
+++ src/main/java/controller/WebController.java
@@ -51,7 +51,7 @@ public class WebController {
 
     private String getPage() {
         StringBuilder sb = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/java/view/html/index.html"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/index.html"))) {
             String line = reader.readLine();
 
             while (line != null) {
