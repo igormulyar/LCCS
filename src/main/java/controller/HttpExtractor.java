3d4ef8c13e33830b4ec0434728420d5420d6fddc
max <farsikovmax@gmail.com>
2016-12-17 19:18:46 +0200

    refactor: use streams, short request mappings, autowire components

--- src/main/java/controller/HttpExtractor.java
+++ src/main/java/controller/HttpExtractor.java
@@ -3,10 +3,8 @@ package controller;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,28 +12,32 @@ import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
 import model.Court;
 import model.CourtCase;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Component
 public class HttpExtractor {
 
-    public List<CourtCase> extractCourtCases(List<String> allIds) {
+    private final ObjectMapper mapper;
 
-        List<CourtCase> resultCaseList = new ArrayList<>();
-        for (String caseNumber : allIds) {
-            Court court = getCourtForRequest(caseNumber); //fetch required headers for http request (collected in controller.HttpExtractor.Court) using case number
-            List<CourtCase> caseList = null;
-            caseList = getCourtCases(court.getUrl(), court.getHost(), court.getReferer(), court.getCourtId());
-            resultCaseList.addAll(caseList.stream()
-                    .filter(currentCase -> currentCase.getNumber().equals(caseNumber))
-                    .collect(Collectors.toList()));
-        }
-        return resultCaseList;
-        // done? please, check// TODO avoid this (null values)
+    @Autowired
+    public HttpExtractor(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public List<CourtCase> extractCourtCases(List<String> allIds) {
+        return allIds.stream()
+                     .map(this::getCourtForRequest)
+                     .map(this::getCourtCases)
+                     .flatMap(List::stream)
+                     .filter(courtCase -> allIds.contains(courtCase.getNumber()))
+                     .collect(Collectors.toList());
     }
 
     /*
@@ -43,47 +45,41 @@ public class HttpExtractor {
     url, referer-header and courtId are parameters used in order to make a correct request
     returns a list of court cases fetched from server
     */
-    private List<CourtCase> getCourtCases(String url, String host, String referer, String courtId) { //I've just removed throwing that IOException from method signature//TODO: catch this exception
+    private List<CourtCase> getCourtCases(Court court) {
         HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(
                 HttpClientBuilder.create().build());
         RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
         ObjectMapper mapper = new ObjectMapper();
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Host", host);
+        headers.set("Host", court.getHost());
         headers.set("Accept", "application/json, text/javascript, */*; q=0.01");
         headers.set("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3");
         headers.set("Accept-Encoding", "gzip, deflate");
         headers.set("Content-Type", "application/x-www-form-urlencoded");
         headers.set("X-Requested-With", "XMLHttpRequest");
-        headers.set("Referer", referer);
-        String body = "q_court_id=" + courtId;
+        headers.set("Referer", court.getReferer());
+        String body = "q_court_id=" + court.getCourtId();
 
         HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
-        List<CourtCase> cases;
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(court.getUrl(), HttpMethod.POST, httpEntity, String.class);
         try {
-            cases = Arrays.asList(mapper.readValue(response.getBody(), CourtCase[].class));
+            return Arrays.asList(mapper.readValue(response.getBody(), CourtCase[].class));
         } catch (IOException e) {
             throw new RuntimeJsonMappingException(e.getMessage());
         }
-        return cases;
     }
 
     private Court getCourtForRequest(String caseNumber) {
-        //done //TODO: I do not understand why you have picked an XML for this. JSON is more convenient, and Jackson can do all this job automatically.
-        //or even simpler way: use java serialization
-        ObjectMapper mapper = new ObjectMapper();
-        List<Court> courts;
         try {
-            courts = Arrays.asList(mapper.readValue(new File(getClass().getResource("/props/courts.json").toURI()), Court[].class));
+            File file = new File(getClass().getResource("/props/courts.json").toURI());
+            String courtId = caseNumber.substring(0, 3);
+            return Arrays.stream(mapper.readValue(file, Court[].class))
+                         .filter(c -> c.getIdInNumber().equals(courtId))
+                         .findAny()
+                         .get();
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeJsonMappingException(e.getMessage());
         }
-        //done//TODO Filtering STREAM !!!
-        Optional<Court> crt = courts.stream()
-                .filter(c -> c.getIdInNumber().equals(caseNumber.substring(0, 3)))
-                .findAny();
-        return crt.get();  //done. if value == null, get() will throw new NoSuchElementException("No value present")  // TODO: really bad thing. If this is valid case use Optional<Court> as return type. If this is exceptional situation - throw an exception.
     }
 }
